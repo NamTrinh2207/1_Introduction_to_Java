2a2ddd94a44b2dc30a9e72c6d929182249035404
unknown <trinhnam180697@gmail.com>
2023-01-06 22:37:44 +0700

    [Bài tập] Ứng dụng đọc số thành chữ

--- read-number/src/Main.java
+++ read-number/src/Main.java
@@ -1,77 +1,84 @@
+// chuyển số thành chữ với 3 chữ số
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter a number : ");
-        String numberInput = sc.nextLine();
+        String numberInput = input.nextLine();
 
-        String M = "một";
-        String H = "hai";
-        String B = "ba";
-        String B1 = "bốn";
-        String N = "năm";
-        String S = "sáu";
-        String B2 = "bảy";
-        String T = "tám";
-        String C = "chín";
-        String K = "không";
+        String[] arrNumberDefault = {"không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín"};
+        String[] arrNumberInput = numberInput.split("");
 
-        String arrNumberDefault[] = new String[]{K, M, H, B, B1, N, S, B2, T, C};
-
-        String arrNumberInput[] = numberInput.split("");
-
-        String resultOne = "";
-        String resultTwo = "";
-        String resultThree = "";
-
+        String indexOne = "";
+        String indexTwo = "";
+        String indexThree = "";
 
         for (int i = 0; i < arrNumberDefault.length; i++) {
             if (arrNumberInput[0].equals(i + "")) {
-                resultOne = arrNumberDefault[i];
-
+                indexOne = arrNumberDefault[i];
             }
-            if (arrNumberInput.length == 2) {
-                if (arrNumberInput[1].equals(i + "")) {
-                    resultTwo = arrNumberDefault[i] + "";
 
+            if (arrNumberInput.length == 2) {
+                if (arrNumberInput[0].equals(i + "")) {
+                    indexOne = arrNumberDefault[i];
+                }
+                if (arrNumberInput[1].equals(i + "")) {
+                    indexTwo = arrNumberDefault[i] + "";
                 }
             }
-            if (arrNumberInput.length == 3) {
-                if (arrNumberInput[2].equals(i + "")) {
-                    resultThree = arrNumberDefault[i] + "";
 
+            if (arrNumberInput.length == 3) {
+                if (arrNumberInput[0].equals(i + "")) {
+                    indexOne = arrNumberDefault[i];
+                }
+                if (arrNumberInput[1].equals(i + "")) {
+                    indexTwo = arrNumberDefault[i] + "";
+                }
+                if (arrNumberInput[2].equals(i + "")) {
+                    indexThree = arrNumberDefault[i] + "";
                 }
             }
         }
         if (arrNumberInput.length == 1) {
-            System.out.println(resultOne);
+            System.out.println(indexOne);
         }
 
         if (arrNumberInput.length == 2) {
-
-            if (resultOne.equals("một") && resultTwo.equals("không")) {
+            if (indexOne.equals("một") && indexTwo.equals("không")) {
                 System.out.print(" mười ");
-
-            } else if (resultOne == "một" && resultTwo != "không") {
-                System.out.print(" mười " + resultTwo);
-
-            } else if (resultTwo.equals("không")) {
-                System.out.print(resultOne + " mươi ");
+            } else if (indexOne.equals("một")) {
+                System.out.print(" mười " + indexTwo);
+            } else if (indexTwo.equals("năm")) {
+                System.out.println(indexOne + " mươi lăm ");
+            } else if (indexTwo.equals("không")) {
+                System.out.print(indexOne + " mươi ");
+            } else if (indexTwo.equals("một")) {
+                System.out.println(indexOne + " mươi mốt ");
             } else {
-                System.out.print(resultOne + " mươi " + resultTwo);
+                System.out.print(indexOne + " mươi " + indexTwo);
             }
         }
 
         if (arrNumberInput.length == 3) {
-
-            if (resultThree == "không") {
-                System.out.print(resultOne + "trăm" + resultTwo + "mươi");
+            if (indexTwo.equals("không") && indexThree.equals("không")) {
+                System.out.println(indexOne + " trăm ");
+            } else if (indexTwo.equals("không")) {
+                System.out.println(indexOne + " trăm lẻ " + indexThree);
+            } else if (indexTwo.equals("một") && indexThree.equals("không")) {
+                System.out.println(indexOne + " trăm mười");
+            } else if (indexTwo.equals("một")) {
+                System.out.println(indexOne + " trăm mười " + indexThree);
+            } else if (indexThree.equals("không")) {
+                System.out.println(indexOne + " trăm " + indexTwo + " mươi");
+            } else if (indexThree.equals("một")) {
+                System.out.println(indexOne + " trăm " + indexTwo + " mươi mốt ");
+            } else if (indexThree.equals("năm")) {
+                System.out.println(indexOne + " trăm " + indexTwo + " mươi lăm ");
             } else {
-                System.out.print(resultOne + "trăm" + resultTwo + "mươi" + resultThree);
+                System.out.println(indexOne + " trăm " + indexTwo + " mươi " + indexThree);
             }
         }
-
-
     }
 }
